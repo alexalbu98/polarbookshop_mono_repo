a8793e1dca7adcc4cc66e19a23a50cc90942ccd1
Alex Albu <aalbu@ssnc-corp.global>
2025-10-18 17:41:09 +0300

    Created mono repo for polar bookshop

---
+++ catalog-service/src/test/java/me/alex/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package me.alex.polarbookshop.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
