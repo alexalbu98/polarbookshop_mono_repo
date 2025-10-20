9bf240049d77aa3217b3ba8d2dc5b060221ccf34
Alex Albu <aalbu@ssnc-corp.global>
2025-10-20 22:06:01 +0300

    Fixed pmd error in catalog-service

--- catalog-service/src/main/java/me/alex/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
+++ catalog-service/src/main/java/me/alex/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
@@ -10,32 +10,32 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class InMemoryBookRepository implements BookRepository {
-    private static final Map<String, Book> books = new ConcurrentHashMap<>();
+    private static final Map<String, Book> BOOKS = new ConcurrentHashMap<>();
 
     @Override
     public Iterable<Book> findAll() {
-        return books.values();
+        return BOOKS.values();
     }
 
     @Override
     public Optional<Book> findByIsbn(String isbn) {
-        return existsByIsbn(isbn) ? Optional.of(books.get(isbn)) :
+        return existsByIsbn(isbn) ? Optional.of(BOOKS.get(isbn)) :
                 Optional.empty();
     }
 
     @Override
     public boolean existsByIsbn(String isbn) {
-        return books.get(isbn) != null;
+        return BOOKS.get(isbn) != null;
     }
 
     @Override
     public Book save(Book book) {
-        books.put(book.isbn(), book);
+        BOOKS.put(book.isbn(), book);
         return book;
     }
 
     @Override
     public void deleteByIsbn(String isbn) {
-        books.remove(isbn);
+        BOOKS.remove(isbn);
     }
 }
