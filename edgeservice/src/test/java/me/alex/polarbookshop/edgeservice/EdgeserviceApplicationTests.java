b67e0a1380c6c8fc88385e8818a6fe761a5ff2ba
Alex Albu <aalbu@ssnc-corp.global>
2025-10-30 14:56:10 +0200

    Added edge service to project

---
+++ edgeservice/src/test/java/me/alex/polarbookshop/edgeservice/EdgeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alex.polarbookshop.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
