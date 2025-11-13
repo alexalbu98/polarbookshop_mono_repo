f3f3b4e83a0baa428eb0991171b11d1f9930db03
Alex Albu <aalbu@ssnc-corp.global>
2025-11-14 00:00:42 +0200

    Integrated edge service with keycloak

---
+++ edge-service/src/main/java/me/alex/polarbookshop/edgeservice/user/UserController.java
@@ -0,0 +1,29 @@
+package me.alex.polarbookshop.edgeservice.user;
+
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @GetMapping("user")
+    public Mono<User> getUser() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(authentication -> {
+                    OidcUser principal = (OidcUser) authentication.getPrincipal();
+                    return new User(
+                            principal.getPreferredUsername(),
+                            principal.getGivenName(),
+                            principal.getFamilyName(),
+                            List.of("employee", "customer")
+                    );
+                });
+    }
+}
