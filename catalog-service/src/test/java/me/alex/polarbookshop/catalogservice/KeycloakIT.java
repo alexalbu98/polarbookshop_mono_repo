593f49651f539a84251097294b7022cfa72fb3be
Alex Albu <aalbu@ssnc-corp.global>
2025-11-18 14:05:14 +0200

    Created keycloak it test

---
+++ catalog-service/src/test/java/me/alex/polarbookshop/catalogservice/KeycloakIT.java
@@ -0,0 +1,20 @@
+package me.alex.polarbookshop.catalogservice;
+
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+
+public interface KeycloakIT {
+
+    @Container
+    KeycloakContainer keycloakContainer =
+            new KeycloakContainer("quay.io/keycloak/keycloak:19.0")
+                    .withRealmImportFile("test-realm-config.json");
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                () -> keycloakContainer.getAuthServerUrl() + "realms/PolarBookshop");
+    }
+}
--- catalog-service/src/test/java/me/alex/polarbookshop/catalogservice/web/BookControllerMvcTests.java
+++ catalog-service/src/test/java/me/alex/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -1,24 +1,54 @@
 package me.alex.polarbookshop.catalogservice.web;
 
+import me.alex.polarbookshop.catalogservice.config.WebSecurityConfig;
 import me.alex.polarbookshop.catalogservice.domain.BookNotFoundException;
 import me.alex.polarbookshop.catalogservice.domain.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
+@Import({WebSecurityConfig.class})
 class BookControllerMvcTests {
     @Autowired
     private MockMvc mockMvc;
 
     @MockitoBean
     private BookService bookService;
+
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
+
+    @Test
+    void whenDeleteBookWithEmployeeRoleThenShouldReturn204() throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+                .perform(MockMvcRequestBuilders.delete("/books/" + isbn)
+                        .with(SecurityMockMvcRequestPostProcessors.jwt()
+                                .authorities(new SimpleGrantedAuthority("ROLE_employee"))))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void whenDeleteBookWithCustomerRoleThenShouldReturn403() throws Exception {
+        var isbn = "7373731394";
+        mockMvc.perform(MockMvcRequestBuilders.delete("/books/" + isbn)
+                        .with(SecurityMockMvcRequestPostProcessors.jwt()
+                                .authorities(new SimpleGrantedAuthority("ROLE_customer"))))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
 
     @Test
     void whenGetBookNotExistingThenShouldReturn404() throws Exception {
