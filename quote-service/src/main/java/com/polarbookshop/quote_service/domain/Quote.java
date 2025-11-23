57d43616fb7bd018cd2963f2085036519927acd2
Alex Albu <aalbu@ssnc-corp.global>
2025-11-23 17:17:58 +0200

    Fixed keycloak https required

---
+++ quote-service/src/main/java/com/polarbookshop/quote_service/domain/Quote.java
@@ -0,0 +1,7 @@
+package com.polarbookshop.quote_service.domain;
+
+public record Quote (
+        String content,
+        String author,
+        Genre genre
+){}
--- quote-service/src/test/java/com/polarbookshop/quote_service/QuoteServiceApplicationTests.java
+++ quote-service/src/test/java/com/polarbookshop/quote_service/QuoteServiceApplicationTests.java
@@ -1,13 +1,41 @@
 package com.polarbookshop.quote_service;
 
+import com.polarbookshop.quote_service.domain.Genre;
+import com.polarbookshop.quote_service.domain.Quote;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class QuoteServiceApplicationTests {
+    @Autowired
+    WebTestClient webTestClient;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void whenAllQuotesThenReturn() {
+        webTestClient.get().uri("/quotes")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBodyList(Quote.class);
+    }
 
+    @Test
+    void whenRandomQuoteThenReturn() {
+        webTestClient.get().uri("/quotes/random")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Quote.class);
+    }
+
+    @Test
+    void whenRandomQuoteByGenreThenReturn() {
+        webTestClient.get().uri("/quotes/random/FANTASY")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Quote.class)
+                .value(quote -> assertThat(quote.genre()).isEqualTo(Genre.FANTASY));
+    }
 }
